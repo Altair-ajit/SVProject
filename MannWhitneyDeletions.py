21dc7f0cce98a04997201a335030602741cffe41
altairajit <rajitzp@gmail.com>
2019-07-02 11:34:21 -0400

    moving files into repo

---
+++ MannWhitneyDeletions.py
@@ -0,0 +1,59 @@
+import pandas as pd
+from pandas import DataFrame as df
+from scipy import stats
+import numpy as np
+import statsmodels
+import bmiList
+from statsmodels.stats.multitest import multipletests as mpt
+
+#initiating the file to be read
+deletions = "dsgv.df"
+dfile = pd.read_pickle(deletions)
+
+#mann whitney for the columns
+ageppl = bmiList.getAgeppl()
+bmippl = bmiList.getBMIppl()
+
+bmiOrder = []
+ageOrder = []
+
+for rows in range(dfile.shape[0]):
+    rname = dfile.index[rows]
+    if dfile.index[rows] in bmippl:
+        bmiOrder.append(bmiList.getBMI(rname))
+    if dfile.index[rows] in ageppl:
+        ageOrder.append(bmiList.getAge(rname))
+
+bmiUs = []
+bmiPvals = []
+
+ageUs = []
+agePvals = []
+
+
+for column in range(dfile.shape[1]):
+    bmiDelArray = []
+    ageDelArray = []
+    for rows in range(dfile.shape[0]):
+        adding = dfile.iloc[rows][dfile.columns[column]]
+        if dfile.index[rows] in bmippl:
+            if pd.isna(adding):
+                bmiDelArray.append(0)
+            else:
+                bmiDelArray.append(adding)
+        if dfile.index[rows] in ageppl:
+            if pd.isna(adding):
+                ageDelArray.append(0)
+            else:
+                ageDelArray.append(adding)
+    u, pVal = stats.mannwhitneyu(bmiOrder, bmiDelArray)
+    bmiPvals.append(pVal)
+    bmiUs.append(u) 
+
+    u, pVal = stats.mannwhitneyu(ageOrder, ageDelArray)
+    agePvals.append(pVal)
+    ageUs.append(u) 
+
+bmiReject, bmiQvals, e, a = mpt(bmiPvals, 0.05, 'fdr_bh', False, False)
+
+bmiReject, bmiQvals, e, a = mpt(bmiPvals, 0.05, 'fdr_bh', False, False)
---
+++ SpearmansVariable.py
@@ -0,0 +1,69 @@
+import pandas as pd
+from pandas import DataFrame as df
+from scipy import stats
+import numpy as np
+import statsmodels
+import bmiList
+from statsmodels.stats.multitest import multipletests as mpt
+
+#initiating the file to be read
+variables = "vsgv.df"
+vfile = pd.read_pickle(variables)
+vfile.to_csv('check.csv')
+
+
+ageppl = bmiList.getAgeppl()
+bmippl = bmiList.getBMIppl()
+
+bmiOrder = []
+ageOrder = []
+
+for rows in range(vfile.shape[0]):
+    rname = vfile.index[rows]
+    if vfile.index[rows] in bmippl:
+        bmiOrder.append(bmiList.getBMI(rname))
+    if vfile.index[rows] in ageppl:
+        ageOrder.append(bmiList.getAge(rname))
+
+#spearman for the columns
+
+bmiRHO = []
+bmiPvals = []
+
+
+ageRHO = []
+agePvals = []
+
+for column in range(vfile.shape[1]):
+    bmiVarArray = []
+    ageVarArray = []
+    for rows in range(vfile.shape[0]):
+        adding = vfile.iloc[rows][vfile.columns[column]]
+        if vfile.index[rows] in bmippl:
+            if pd.isna(adding):
+                bmiVarArray.append(0)
+            else:
+                bmiVarArray.append(adding)
+        if vfile.index[rows] in ageppl:
+            if pd.isna(adding):
+                ageVarArray.append(0)
+            else:
+                ageVarArray.append(adding)
+    
+    bmiSpear = np.stack((bmiOrder, bmiVarArray))
+    rho, pval = stats.spearmanr(bmiSpear)
+    bmiRHO.append(rho)
+    bmiPvals.append(pval)
+
+    ageSpear = np.column_stack((ageOrder, ageVarArray))
+    rho, pval = stats.spearmanr(ageSpear)
+    ageRHO.append(rho)
+    agePvals.append(pval)
+
+
+
+bmiReject, bmiQvals, e, a = mpt(bmiPvals, 0.05, 'fdr_bh', False, False)
+
+ageReject, ageQvals, e, a = mpt(agePvals, 0.05, 'fdr_bh', False, False)
+
+#heatmap condtitions
